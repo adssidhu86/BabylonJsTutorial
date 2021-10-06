ed325f9cac1d3d67bd6db5547da9788692a8d1b8
AIEdX <32054618+adssidhu86@users.noreply.github.com>
2021-10-06 13:04:41 +0530

    changes to main js with box and sphere using mesh builder

--- js/main.js
+++ js/main.js
@@ -14,10 +14,19 @@ const scene = new BABYLON.Scene(engine);
 
 //create a camera 
 //@param name= "camera1"
-const camera = new BABYLON.FreeCamera('camera',new BABYLON.Vector3(0,0,0),scene);
+const camera = new BABYLON.FreeCamera('camera',new BABYLON.Vector3(0,0,-10),scene);
 //create a light 
 
-const light = new BABYLON.HemisphericLight('light',new BABYLON.vector3(0,1,0),scene);
+const light = new BABYLON.HemisphericLight('light',new BABYLON.Vector3(0,1,0),scene);
+
+//create a box
+const box =  BABYLON.MeshBuilder.CreateBox('box', {}, scene);
+
+//create a sphere
+const sphere =  BABYLON.MeshBuilder.CreateSphere('sphere',{}, scene);
+sphere.position = new BABYLON.Vector3(3,0,0)
+
+
 
 return scene;
 }
