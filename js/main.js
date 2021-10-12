724f0ce8b76405004749d912f0b1ac15e84d7c95
AIEdX <32054618+adssidhu86@users.noreply.github.com>
2021-10-12 13:59:28 +0530

    changes to main js

--- js/main.js
+++ js/main.js
@@ -16,14 +16,20 @@ const scene = new BABYLON.Scene(engine);
 
 //create a camera 
 //@param name= "camera1"
-const camera = new BABYLON.FreeCamera('camera',new BABYLON.Vector3(0,0,-10),scene);
+//const camera = new BABYLON.FreeCamera('camera',new BABYLON.Vector3(0,0,-10),scene);
+const camera = new BABYLON.UniversalCamera('camera',,)
 camera.attachControl(canvas,true);
-//create a light 
+//create a 
 
-const light = new BABYLON.HemisphericLight('light',new BABYLON.Vector3(0,1,0),scene);
+//const light = new BABYLON.HemisphericLight('light',new BABYLON.Vector3(0,1,0),scene);
+//const light = new BABYLON.PointLight('light',new BABYLON.Vector3(0,5,0),scene);
+const light = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(5,-1,0),scene);
 
 //create a box
 const box =  BABYLON.MeshBuilder.CreateBox('box', {}, scene);
+box.rotation.x =2;
+box.rotation.x =0.5;
+box.rotation.x =-0.7;
 
 //create a sphere
 const sphere =  BABYLON.MeshBuilder.CreateSphere('sphere',{
@@ -31,7 +37,7 @@ const sphere =  BABYLON.MeshBuilder.CreateSphere('sphere',{
     diameter:2,
 }, scene);
 sphere.position = new BABYLON.Vector3(3,0,0);
-
+sphere.scaling = new BABYLON.Vector3(0.5,0.5,0.5);
 // create a plane 
 const plane = BABYLON.MeshBuilder.CreatePlane('plane',{},scene);
 plane.position= new BABYLON.Vector3(-3,0,0);
@@ -45,6 +51,16 @@ const points = [
 ];
 
 const lines = BABYLON.MeshBuilder.CreateLines('lines',{points},scene);
+
+//create materials 
+const material = new BABYLON.StandardMaterial('material',scene);
+material.diffuseColor = new BABYLON.Color3(1,3,0);
+material.emissiveColor =new BABYLON.Color3(0,1,0);
+box.material= material;
+
+const material2 = new BABYLON.StandardMaterial('material2',scene);
+material2.diffuseTexture = new BABYLON.Texture('assets/images/dark_rock.png', scene);
+sphere.material = material2;
 
 return scene;
 }
